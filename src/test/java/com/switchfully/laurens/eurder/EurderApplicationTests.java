ab6d06fa083b5062086f62b2489fc509ce37f264
laurensvdb95 <vandenbergh.laurens@hotmail.com>
2020-10-28 10:04:29 +0100

    initial commit project setup

---
+++ src/test/java/com/switchfully/laurens/eurder/EurderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.laurens.eurder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
